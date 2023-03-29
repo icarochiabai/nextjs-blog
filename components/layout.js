46f224e54a72523ce24091fec710277a92b9dff8
Ícaro Chiabai <i247171@dac.unicamp.br>
2023-03-28 23:47:43 -0300

    fix image bug

--- components/layout.js
+++ components/layout.js
@@ -30,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/nextjs-blog/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
